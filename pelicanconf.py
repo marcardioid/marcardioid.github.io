708d8864c9c94794afce5881bd1430cd2df02275
marcardioid <marc.sleegers@gmail.com>
2026-03-04 16:31:22 +0100

    Optimize FCP

--- pelicanconf.py
+++ pelicanconf.py
@@ -9,6 +9,8 @@ FOOTER_TEXT = f'&copy; {datetime.now().year} Marc Sleegers. Licensed <a href="ht
 
 SITE_AUTHOR = "Marc Sleegers"
 SITE_AUTHOR_TITLE = "Principal Software Engineer"
+ICONS_PATH = "images/icons"
+SOCIAL_IMAGE_FILENAME = "avatar-204.jpg"
 SOCIAL_IMAGE_ALT = "Portrait of Marc Sleegers"
 ENABLE_HOME_IDENTITY_NAME = False
 ROBOTS_INDEXABLE_DEFAULT = "index,follow,max-image-preview:large"
@@ -29,8 +31,6 @@ NAVIGATION_ITEMS = [
     # ("/uses/", "uses", "Uses"),
     # ("/now/", "now", "Now"),
 ]
-
-ICONS_PATH = "images/icons"
 
 GOOGLE_FONTS = [
     "Inter:ital,wght@0,400..900;1,400..900",
